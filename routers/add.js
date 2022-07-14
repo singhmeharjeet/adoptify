3b4a5ba6c26e53cf5b47072d996058aafc9b612c
sha241 <sha241@sfu.ca>
2022-07-14 16:42:27 -0700

    fixed the image uploading

--- routers/add.js
+++ routers/add.js
@@ -2,43 +2,63 @@ const express = require("express"),
 	router = express.Router();
 
 const pool = require("../database");
+const AWS = require('aws-sdk')
+const fs = require('fs')
 
-// const AWS = require('aws-sdk');
-// const fs = require('fs');
-// // AWS.config.loadFromPath('../config.json')
-
-// const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+	region: 'us-west-2',
+	accessKeyId: 'AKIAQPWQC7WAFUD3JVNS',
+	secretAccessKey: '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',
+});
 
 module.exports = router.post("/", async (req, res) => {
 	if (req.files === null) {
 		return res.status(400).json({ msg: "no file uploaded" });
 	}
 
-	const { username, petName, petSpecies, petColor, petDescription } =
-		await req.body;
+	const { username, petName, petSpecies, petColor, petDescription } = req.body;
 	const petImage = req.files.petImage;
+	console.log("Pet Image Object: ", petImage);
+	petImage.mv(`${__dirname}/../uploadedImages/${petImage.name}`, (err) => {
+		if (err) { 
+			console.error(err);
+            return res
+			.status(500)
+			.json({ msg: "can't save file in the server" });
+        }
+ 
+		var imgpath = `${__dirname}/../uploadedImages/${petImage.name}`;
+		console.log("new path name: ", imgpath)
+		const fileContent = fs.readFileSync(imgpath);
+		console.log("got image")  
+
+		//TO GIVE IMAGE A UNIQUE ID
+		//insert post with pet name, pet species, color, description, username, INSERT query
+		//find that post we just inserted, extract post id, SELECT query
+		// name it with that post id,
+
+		const params = { 
+			Bucket: "adoptifybucket",
+			Key: `${username}.jpg`,
+			Body: fileContent, 
+		};
+		s3.upload(params, async function (err, data) { 
+			if (err) console.log(err);
+			var petImageURL = data.Location;
+			//update query and add the petimage url, UPDATE query
+			const newPostQ = `INSERT INTO posts(pet_name, pet_species, pet_color, images, description, fk_username) VALUES 
+			('${petName}', '${petSpecies}', '${petColor}', ARRAY ['${petImageURL}'], '${petDescription}', '${username}');`;
+			const request = await pool.query(newPostQ);
+			console.log("File uploaded succesfully,", petImageURL) 
+			return res.status(200).json({
+				msg: "file uploaded successfully",
+				image: petImage,
+				imageName: `${petImage.name}`,
+				imagePath: `${__dirname}/uploadedImages/${petImage.name}`,
+			}); 
+		});
+
+    });
+
 });
 
-// module.exports = router.post("/", async (req, res) => {
-// 	const username = req.body.username;
-// 	const petName = req.body.title;
-// 	const species = req.body.species;
-// 	const colour = req.body.color;
-// 	const description = req.body.description;
-
-// 	var petImageFName = req.body.image;
-// 	const fileContent = fs.readFileSync(petImageFName);
-// 	const params = {
-// 		Bucket: "adoptifybucket",
-// 		Key: "petImage.jpg",
-// 		Body: fileContent,
-// 	};
-// 	s3.upload(params, async function (err, data) {
-// 		if (err) throw err;
-// 		var petImageURL = data.Location;
-// 		const newPostQ = `INSERT INTO posts(pet_name, pet_species, pet_color, images, description, fk_username) VALUES
-//     ('${petName}', '${species}', '${colour}', ARRAY ['${petImageURL}'], '${description}', '${username}');`;
-// 		await pool.query(newPostQ);
-// 	});
-// 	res.send("New post added succesfully!");
-// });
