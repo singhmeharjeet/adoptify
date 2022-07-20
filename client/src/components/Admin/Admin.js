6352290ce0a0e715d0b5dcbd1e5ee348bf8f297c
Jennifer Kim <jennykim029@gmail.com>
2022-07-20 02:42:44 -0700

    Created Admin Page UI
    added admin page

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BASE_URL } from "../constants";
+import NavBar from "../NavBar/NavBar";
+import { useParams, useNavigate} from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUser, faPaw, faChild, faDog, faUsers, faShieldDog} from '@fortawesome/free-solid-svg-icons';
 import "./Admin.css";
 
 // BUGGED NOT WORKING RN, WILL FIX LATER
 // admin page should show all the users in the database, delete certain users
-export default function Admin() {
-	const [allUsers, setAllUsers] = useState("");
-	async function getAllUsers() {
-		let result = "";
-		try {
-			const responseJSON = await (
-				await fetch(`${BASE_URL}/admin`, {
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				})
-			).json();
-			// if response is sucessful
-			result = responseJSON;
-		} catch (error) {
-			console.log("error", error);
-		}
-		return result.rows;
-	}
+export default function Admin({ clearPermission }) {
+	// const [allUsers, setAllUsers] = useState("");
+	// async function getAllUsers() {
+	// 	let result = "";
+	// 	try {
+	// 		const responseJSON = await (
+	// 			await fetch(`${BASE_URL}/admin`, {
+	// 				method: "GET",
+	// 				headers: {
+	// 					"Content-Type": "application/json",
+	// 				},
+	// 			})
+	// 		).json();
+	// 		// if response is sucessful
+	// 		result = responseJSON;
+	// 	} catch (error) {
+	// 		console.log("error", error);
+	// 	}
+	// 	return result.rows;
+	// }
 	//work on adding permission check
 
-	setAllUsers(getAllUsers());
-	console.log("allUsers: ", allUsers);
+	// setAllUsers(getAllUsers());
+	// console.log("allUsers: ", allUsers);
 	// console.log(Array)
 	// var arr = [];
 	// for (var i=0; i<2; i++) {
