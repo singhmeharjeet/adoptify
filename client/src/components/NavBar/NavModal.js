f4593f02c13bb8b4b674d4928d700a5093f5818a
Jennifer Kim <jennykim029@gmail.com>
2022-08-01 23:02:00 -0700

    update-signout-modal

--- client/src/components/NavBar/NavModal.js
+++ client/src/components/NavBar/NavModal.js
@@ -1,22 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faX,
+} from "@fortawesome/free-solid-svg-icons";
+import "./NavBar.css";
 
 export default function NavModal({ handleLogout, exit }) {
 	return (
 		<>
 			<div className="navModal-overlay" onClick={exit} />
 			<div className="navModal">
-				<div className="confirm-row">
+				<div className="modal-confirm-row">
 					Want to Sign Out?
-					<button className="button" onClick={handleLogout}>
+					<button className="modal-signout-button" onClick={handleLogout}>
 						Sign Out
 					</button>
 				</div>
-				<div className="confirm-row">
-					Go Back to the website
-					<button className="button" onClick={exit}>
-						Go Back
-					</button>
-				</div>
+				<FontAwesomeIcon
+							className="signout-close-icon"
+							icon={faX}
+							onClick={exit}
+				></FontAwesomeIcon>
 			</div>
 		</>
 	);
