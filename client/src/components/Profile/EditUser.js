d26685f5c7ca32850e3a345d6b46f43d069c2d4c
Ekamdeep Kaur <eka61@sfu.ca>
2022-08-03 22:03:55 -0700

    editUser  error validation

--- client/src/components/Profile/EditUser.js
+++ client/src/components/Profile/EditUser.js
@@ -12,57 +12,171 @@ export default function EditUser({
 	const [passwordType, setPasswordType] = useState("password");
 	const [confirmPasswordType, setConfirmPasswordType] = useState("password");
 	const [editStatus, setEditStatus] = useState("");
+    const[isError,setError] = useState(false);
+	const [uFirstName, setFirstName] = useState("");
+	const [uLastName, setLastName] = useState("");
+	const [uPhoneNum, setPhoneNum] = useState("");
+	const [uAddress, setAddress] = useState("");
+	const [uPassword, setPassword] = useState("");
+	const [uConfirmPassword, setConfirmPassword] = useState("");
+	const [errFirstName,setErrFirstName] = useState("");
+	const [errLastName,setErrLastName] = useState("");
+	const [errPhoneNum,setErrPhoneNum] = useState("");
+	const [errAddress,setErrAddress] = useState("");
+	const [errPassword,setErrPassword] = useState("");
+	const [errConfPassword,setErrConfPassword] = useState(""); 
 
 	useEffect(() => {
 		setTimeout(() => setEditStatus(""), 4000);
 	}, [editStatus]);
 	
-	async function saveUserDetails() {
-		let pw = document.getElementById("pw").value;
+	const validateFirstName=(event) => {
+		const firstname = event.target.value;
+		const patt = RegExp(/^[A-Za-z0-9]{3,16}$/i);
+		setFirstName(firstname);
+	
+		if(firstname == "")
+		{
+			setErrFirstName("First name is required");
+			
+			setError(true);	
+		}
+		else if(!patt.test(firstname)){
+			setErrFirstName("First Name should be 3-16 characters long and shouldn't include any special character! ")
+			setError(true);	
+		
+		}
+		else{
+			setErrFirstName("");
+			setError(false);	
+		}
+	}
+
+	const validateLastName=(event) => {
+		const lastname = event.target.value;
+		setLastName(lastname);
+		const patt = RegExp(/^[A-Za-z0-9]{2,16}$/i);
+		// console.log(lastname)
+		if(lastname == "")
+		{
+			setErrLastName("Last name is required");
+			setError(true);
+			
+		}
+		else if(!patt.test(lastname)){
+			setErrLastName("Last name should be 2-16 characters long and shouldn't include any special character! ");
+			setError(true);
+			
+		}
+		else{
+			setErrLastName("");
+			setError(false);
+			
+		}
+	}
+
+	const validatePhoneNum=(event) => {
+		const phonenum = event.target.value;
+		setPhoneNum(phonenum);
+		const patt = RegExp(/^\d{3}\d{3}\d{4}$/i);
+		// console.log(phonenum)
+		if(phonenum == "")
+		{
+			setErrPhoneNum("Phone number is required");
+			setError(true);
+		
+		}
+		else if(!patt.test(phonenum)){
+			setErrPhoneNum("Invalid phone number \n10 digit input is required")
+			setError(true);
+		
+		}
+		else{
+			setErrPhoneNum("");
+			setError(false);
+			
+		}
+	}
+	const validateAddress =(event) => {
+		const address = event.target.value;
+		setAddress(address);
+		// console.log(address)
+		if(address== "")
+		{
+			setErrAddress("City should not be empty");
+			setError(true);
+			
+		}
+		else{
+			setErrAddress("");
+			setError(false);
+			
+		}
+	}
+
+	const validatePassword=(event) => {
+		const password= event.target.value;
+		setPassword(password);
 		let c_pw = document.getElementById("c_pw").value;
+		const patt = RegExp(/^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/i);
+		console.log(password)
+		if(password== "")
+		{
+			setErrPassword("Password is required");
+			setError(true);
+			
+		}
+		else if(!patt.test(password)){
+			setErrPassword("Use 8 or more characters with a mix of letters, numbers & symbols ")
+			setError(true);
+			
+		}
+		else if(password !== c_pw){
+			setErrPassword("")
+			setErrConfPassword("Confirm password don't match with Password! Try again.");	
+			setError(true);
+		}
+		
+		else{
+			setErrPassword("");
+			setErrConfPassword("");	
+			setError(false);
+			
+		}
+	}
 
-		if (pw !== c_pw) {
-			window.alert("password and cofirmed password are not the same");
-			return;
+
+
+	const validateConfPassword=(event) => {	
+		let pw = document.getElementById("pw").value;
+		const confirmPassword = event.target.value;
+		setConfirmPassword(confirmPassword);
+		// console.log(confirmPassword)
+		
+		if(confirmPassword !== pw){
+			setErrConfPassword("Confirm password don't match with Password! Try again.");	
+			setError(true );
+		}
+		else {
+			setErrConfPassword("");
+			setError(false);
+			
+		}
+	}
+
+	async function saveUserDetails() {
+		if(errFirstName || errLastName || errAddress || errPhoneNum || errPassword || errConfPassword){
+			return false;
 		}
 
-		var patt = RegExp(/^[A-Za-z0-9]{3,16}$/i);
 		let fname = document.getElementById("fname").value;
-		if (!patt.test(fname)) {
-			window.alert("invalid firstname input\n3-16 letters required");
-			return;
-		}
-
-		patt = RegExp(/^[A-Za-z0-9]{2,16}$/i);
 		let lname = document.getElementById("lname").value;
-		if (!patt.test(lname)) {
-			window.alert("invalid lastname input\n2-16 letters required");
-			return;
-		}
-
-		patt = RegExp(/^\d{3}\d{3}\d{4}$/i);
 		let p_num = document.getElementById("p_num").value;
-		if (!patt.test(p_num)) {
-			window.alert(
-				"invalid phone number input\n10 digit input is required"
-			);
-			return;
-		}
-
-		patt = RegExp(
-			/^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$/i
-		);
-		if (!patt.test(pw)) {
-			window.alert(
-				"invalid password input\nUse 8 or more characters with a mix of letters, numbers & symbols"
-			);
-			return;
-		}
-
 		let address = document
 			.getElementById("address")
 			.value.replace("'", "''");
-
+		let pw = document.getElementById("pw").value;
+		let c_pw = document.getElementById("c_pw").value;
 		const isEdited = await editUserData(
 			userDetails?.username,
 			pw,
@@ -71,6 +185,7 @@ export default function EditUser({
 			p_num,
 			address
 		);
+	
 		return isEdited;
 	}
 
@@ -156,7 +271,18 @@ export default function EditUser({
 												userDetails?.firstname
 											}
 											id="fname"
+											onChange={(event) => {
+												validateFirstName(event);
+												
+											}}
 										/>
+									</td>
+									
+								</tr>
+								<tr className="editUser-row">
+									<td className="editUser-row-right">
+									<div><label className="errMsgEditUser">{errFirstName}
+										</label></div>
 									</td>
 								</tr>
 								<tr className="editUser-row">
@@ -167,7 +293,16 @@ export default function EditUser({
 										<input
 											defaultValue={userDetails?.lastname}
 											id="lname"
+											onChange={(event) => {
+												validateLastName(event)
+											}}
 										></input>
+									</td>
+								</tr>
+								<tr className="editUser-row">
+									<td className="editUser-row-right">
+									<div><label className="errMsgEditUser">{errLastName}
+										</label></div>
 									</td>
 								</tr>
 								<tr className="editUser-row">
@@ -178,7 +313,16 @@ export default function EditUser({
 										<input
 											defaultValue={userDetails?.phone}
 											id="p_num"
+											onChange={(event) => {
+												validatePhoneNum(event)
+											}}
 										></input>
+									</td>
+								</tr>
+								<tr className="editUser-row">
+									<td className="editUser-row-right">
+									<div><label className="errMsgEditUser">{errPhoneNum}
+										</label></div>
 									</td>
 								</tr>
 								<tr className="editUser-row">
@@ -187,7 +331,16 @@ export default function EditUser({
 										<input
 											defaultValue={userDetails?.address}
 											id="address"
+											onChange={(event) => {
+												validateAddress(event)
+											}}
 										></input>
+									</td>
+								</tr>
+								<tr className="editUser-row">
+									<td className="editUser-row-right">
+									<div><label className="errMsgEditUser">{errAddress}
+										</label></div>
 									</td>
 								</tr>
 								<tr className="editUser-row">
@@ -199,6 +352,10 @@ export default function EditUser({
 											id="pw"
 											type={passwordType}
 											defaultValue={userDetails?.password}
+											onChange={(event) => {
+												validatePassword(event)
+												
+											}}
 										></input>
 										<i onClick={handleEyeClick}>
 											{passwordType === "password" ? (
@@ -216,6 +373,12 @@ export default function EditUser({
 									</td>
 								</tr>
 								<tr className="editUser-row">
+									<td className="editUser-row-right">
+									<div><label className="errMsgEditUser">{errPassword}
+										</label></div>
+									</td>
+								</tr>
+								<tr className="editUser-row">
 									<th className="editUser-row-left">
 										Confirm Password:
 									</th>
@@ -223,6 +386,10 @@ export default function EditUser({
 										<input
 											id="c_pw"
 											type={confirmPasswordType}
+											onChange={(event) => {
+												validateConfPassword(event)
+												
+											}}
 											defaultValue={userDetails?.password}
 										></input>
 										<i onClick={handleConfirmEyeClick}>
@@ -239,6 +406,12 @@ export default function EditUser({
 												></FontAwesomeIcon>
 											)}
 										</i>
+									</td>
+								</tr>
+								<tr className="editUser-row">
+									<td className="editUser-row-right">
+									<div><label className="errMsgEditUser">{errConfPassword}
+										</label></div>
 									</td>
 								</tr>
 								<tr className="editUser-btn-wrap">
